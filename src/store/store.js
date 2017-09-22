635e6e01adcf088404893aadacea12d15a37a9cd
Jean-François Bisson <jbisson@uwaterloo.ca>
2017-09-22 12:34:24 -0400

    :moyai: Simplify the logger

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,16 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import reducers from '../reducers';
 
 const middlewares = [thunk];
 if (process.env.NODE_ENV !== 'production') {
-  const logger = createLogger({ collapsed: true });
+  const logger = () => next => (action) => {
+    if (typeof action !== 'function') {
+      console.info('dispatching:', action);
+    }
+    return next(action);
+  };
+
   middlewares.push(logger);
 }
 
